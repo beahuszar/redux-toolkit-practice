c6ffda2858aa26c6e208abbfcd8676cef882a611
beahuszar <beata.huszar@recart.com>
2022-08-16 12:09:05 +0200

    Add redux store

---
+++ src/components/Profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Profile: React.FC = () => {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>Name: </p>
+      <p>Age: </p>
+      <p>Email: </p>
+    </div>
+  )
+}
